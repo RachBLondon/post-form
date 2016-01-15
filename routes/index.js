bea2fce42a9519b3d6f896e3a4cbdc62ee81b478
Rachel Black <rachel.black1984@googlemail.com>
2016-01-15 18:37:02 +0000

    adding addition users to disk storage

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var router  = express.Router();
 var fs      = require("fs");
+var dataJSON = require(__dirname +"/../models/dataStore");
+var count = 0;
+
+console.log("dataJSON....", dataJSON);
+
+
+var dataObject = new Object();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,8 +15,16 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/postForm',function (req, res){
-  var formData = JSON.stringify(req.body);
-  writeToDisk(formData);
+
+  var formData = req.body;
+  dataObject["person"+ count] = formData;
+  console.log("????",formData);
+  console.log("LLL",dataObject);
+  count ++;
+  console.log(count);
+
+
+  writeToDisk(JSON.stringify(dataObject));
 });
 
 
